ad84157d4368027e1b15b37403a512d542ff89d0
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2025-10-06 23:49:26 -0600

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.next();
   } catch (err) {
     console.error("❌ Token inválido:", err);
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 
